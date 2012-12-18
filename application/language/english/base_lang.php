28c89d5e1dbba9a0d45366f092b6c2509400bf68
root <root@vaiste.com>
2012-12-18 21:30:39 +0200

    Added English translation

---
+++ application/language/english/base_lang.php
@@ -0,0 +1,51 @@
+<?php
+
+$lang['base_login'] = "Login";
+$lang['base_username'] = "Username";
+$lang['base_password'] = "Password";
+$lang['base_wrong_username_password'] = "Wrong username or password";
+$lang['base_code'] = "Code";
+$lang['base_seat'] = "Seat";
+$lang['base_password_again'] = "Password again";
+$lang['base_email'] = "Email";
+$lang['base_username_created'] = "Username created! You may now login.";
+$lang['base_register'] = "Register";
+$lang['base_incorrect_code'] = "Incorrect code or your seat has already been reserved. Please contact administration!";
+$lang['base_you_need_code'] = "You need a code to register your seat. If you do not have a code, please ask for it at the administration desk!";
+$lang['base_logout'] = "Logout";
+$lang['base_hello'] = "Hey";
+$lang['base_send'] = "Send";
+$lang['base_search'] = "Search";
+$lang['base_close'] = "Close";
+$lang['base_loading'] = "Loading...";
+
+$lang['base_dashboard'] = "Dashboard";
+$lang['base_schedule'] = "Schedule";
+$lang['base_tournaments'] = "Tournaments";
+$lang['base_pizza'] = "Pizza";
+$lang['base_map'] = "Map";
+$lang['base_song_requests'] = "Song Requests";
+
+$lang['base_weekday_1'] = "Monday";
+$lang['base_weekday_2'] = "Tuesday";
+$lang['base_weekday_3'] = "Wednesday";
+$lang['base_weekday_4'] = "Thursday";
+$lang['base_weekday_5'] = "Friday";
+$lang['base_weekday_6'] = "Saturday";
+$lang['base_weekday_7'] = "Sunday";
+$lang['base_open_irc'] = "Open IRC";
+$lang['base_send_song_request'] = "Send a song request";
+$lang['base_suggest_song'] = "Suggest a song";
+$lang['base_song_in_spotify'] = "Song is on Spotify";
+$lang['base_song_not_in_spotify'] = "Song is not on Spotify";
+$lang['base_song_name'] = "Song name";
+$lang['base_album'] = "Album";
+$lang['base_artist'] = "Artist";
+$lang['base_type_spotify_url'] = "Type Spotify URL";
+$lang['base_thanks_song_request'] = "Thank you for your song request!";
+$lang['base_incorrect_url'] = "Incorrect URL!";
+
+$lang['base_participants'] = "Participants";
+$lang['base_participate'] = "Participate";
+
+$lang['base_seats'] = "Seatmap";
